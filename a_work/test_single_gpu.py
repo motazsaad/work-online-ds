4adb1fb3e2672570271d2b2d587fa13e30d6b0ab
Abdullrahman ElHayek <ahayek84@gmail.com>
2019-10-07 16:05:06 +0300

    abed work on collection 2

---
+++ a_work/test_single_gpu.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+
+
+tf.debugging.set_log_device_placement(True)
+
+# Create some tensors
+a = tf.constant([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
+b = tf.constant([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]])
+c = tf.matmul(a, b)
+
+print(c)
+
+print(tf.__version__)
